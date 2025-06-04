f022c7158aad0100f28e640f78e12a6953ea872c
Steve S. <J294862@tafe.wa.edu.au>
2025-06-04 19:16:45 +0800

    created React app for Frontend

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
-router.get('/logout', logout)
+router.get('/logout', protectedRoute, logout)
 router.post('/refresh', refreshToken)
 router.get('/profile', protectedRoute, getProfile)
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 font-bold text-5xl'>Hello !!</h1>
+    </>
+  )
+}
+
+export default App
