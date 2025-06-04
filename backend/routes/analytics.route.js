1e8e99564521644fe1d9f127b30a632261afd2da
Steve S. <J294862@tafe.wa.edu.au>
2025-06-04 12:37:46 +0800

    feat: Analytics routes added

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,8 +1,20 @@
 import express from 'express'
-import {} from '../controllers/analytics.controller.js'
-import { protectedRoute } from '../middleware/auth.middleware.js'
+import { getAnalyticsData, getDailySalesData } from '../controllers/analytics.controller.js'
+import { adminRoute, protectedRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
+router.get('/', protectedRoute, adminRoute, async (req, res) => {
+  try {
+    const analyticsData = await getAnalyticsData()
+    const endDate = new Date()
+    const startDate = new Date(endDate.getTime() - 604800000)
+    const dailySalesData = await getDailySalesData(startDate, endDate)
+
+    res.json({analyticsData, dailySalesData})
+  } catch (err) {
+    res.status(500).json({success: false, message: `Error in Analytics route: ${err.message}`})
+  }
+})
 
 export { router }
